1a9b970ec1204d1613d974bf6bb6c7d604d68f28
Savvas Sheittanis <sheittanis@hotmail.com>
2019-09-03 22:41:55 +0100

    Updates

---
+++ precache-manifest.c37d7cc27001ca8cd5131d49a8b55cdc.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf1fab6e1d9995fb8fce68f5181795be",
+    "url": "/rock-paper-scissors/index.html"
+  },
+  {
+    "revision": "7ddd3fca4097ad7eaf75",
+    "url": "/rock-paper-scissors/static/css/2.181f3b12.chunk.css"
+  },
+  {
+    "revision": "b6b49a8cc375b6677a04",
+    "url": "/rock-paper-scissors/static/css/main.f09c3297.chunk.css"
+  },
+  {
+    "revision": "7ddd3fca4097ad7eaf75",
+    "url": "/rock-paper-scissors/static/js/2.a78cdd93.chunk.js"
+  },
+  {
+    "revision": "b6b49a8cc375b6677a04",
+    "url": "/rock-paper-scissors/static/js/main.f7f61ee4.chunk.js"
+  },
+  {
+    "revision": "d2f53215bedb4611a28e",
+    "url": "/rock-paper-scissors/static/js/runtime~main.75db9d22.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/rock-paper-scissors/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/rock-paper-scissors/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/rock-paper-scissors/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/rock-paper-scissors/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/rock-paper-scissors/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/rock-paper-scissors/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/rock-paper-scissors/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/rock-paper-scissors/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/rock-paper-scissors/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/rock-paper-scissors/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/rock-paper-scissors/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/rock-paper-scissors/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/rock-paper-scissors/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/rock-paper-scissors/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/rock-paper-scissors/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/rock-paper-scissors/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.78227e4038bc4819b471e9823a4e842b.js"
+  "/rock-paper-scissors/precache-manifest.c37d7cc27001ca8cd5131d49a8b55cdc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rock-paper-scissors/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
